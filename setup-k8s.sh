76bceefa7bc1c51e3d45130411653fdd959c21ee
Vincent Lesierse <vincent@lesierse.com>
2022-06-22 09:45:52 +0000

    Make K8s setup explicit

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -7,13 +7,13 @@ export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION='eu-west-1'
 
 # Create Amazon EKS Cluster
-eksctl create cluster -f cluster.yaml
+eksctl create cluster -f ./k8s/cluster.yaml
 eksctl create iamidentitymapping --cluster awsdemo-hpa --arn arn:aws:iam::036828455238:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_AWSAdministratorAccess_b3648c2b53da13ae --group system:masters --username admin
 
 # Install Cluster Auto Scaling
 aws iam create-policy   \
   --policy-name AWSDemoHPA-KubernetesAutoScalingPolicy \
-  --policy-document file://cluster-autoscaler-policy.json
+  --policy-document file://k8s/cluster-autoscaler-policy.json
 
 eksctl create iamserviceaccount \
     --name cluster-autoscaler \
@@ -23,14 +23,14 @@ eksctl create iamserviceaccount \
     --approve \
     --override-existing-serviceaccounts
 
-kubectl apply -f cluster-autoscaler.yaml
+kubectl apply -f ./k8s/cluster-autoscaler.yaml
 kubectl -n kube-system annotate deployment.apps/cluster-autoscaler cluster-autoscaler.kubernetes.io/safe-to-evict="false"
 
 
 # Install Kubernetes CloudWatch Adapter
 aws iam create-policy   \
   --policy-name AWSDemoHPA-KubernetesCloudWatchAdapterPolicy \
-  --policy-document file://k8s-cloudwatch-adapter-policy.json
+  --policy-document file://k8s/k8s-cloudwatch-adapter-policy.json
   
 eksctl create iamserviceaccount \
     --name k8s-cloudwatch-adapter \
@@ -39,6 +39,6 @@ eksctl create iamserviceaccount \
     --attach-policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/AWSDemoHPA-KubernetesCloudWatchAdapterPolicy" \
     --approve \
     --override-existing-serviceaccounts
-kubectl apply -f k8s-cloudwatch-adapter.yaml
+kubectl apply -f ./k8s/k8s-cloudwatch-adapter.yaml
 
 kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/orders-queue-length" | jq '.'
